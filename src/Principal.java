b3c48ffd3aa8da2957a10b9ad6ee1f2297e2b395
Matheus <mthsilva1998@gmail.com>
2024-08-17 21:47:17 -0300

    estudos do dia 17/08/2024

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -9,12 +11,37 @@ public class Principal {
 
 
         meuFilme.exibeFichaTecnica();
-
+        System.out.println("Duração do filme " +meuFilme.getDuracaoEmMinutos());
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println("Get total de avaliações " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("total de avaliações " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2005);
+        lost.exibeFichaTecnica();
+        lost.setTempradas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("A trança do rei careca");
+        outroFilme.setAnoDeLancamento(2024);
+        outroFilme.setDuracaoEmMinutos(200);
+
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+
 
 
 
---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    private int tempradas;
+    private boolean ativa;
+    private int episodioPorTemporada;
+    private int minutosPorEpisodio;
+
+    public int getTempradas() {
+        return tempradas;
+    }
+
+    public void setTempradas(int tempradas) {
+        this.tempradas = tempradas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodioPorTemporada() {
+        return episodioPorTemporada;
+    }
+
+    public void setEpisodioPorTemporada(int episodioPorTemporada) {
+        this.episodioPorTemporada = episodioPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return tempradas * episodioPorTemporada * minutosPorEpisodio;
+    }
+}
