e6a10e3e2ce59d681265467aba34c0f278dcf3b4
Matheus <mthsilva1998@gmail.com>
2024-08-22 14:18:40 -0300

    Update TesteVendavel.java

--- src/vendavel/TesteVendavel.java
+++ src/vendavel/TesteVendavel.java
@@ -4,7 +4,7 @@ public class TesteVendavel {
     public static void main(String[] args) {
         Produto produto = new Produto("Sapato", 69.90);
 
-        produto.precoTotalComDesconto()
+
 
 
     }
