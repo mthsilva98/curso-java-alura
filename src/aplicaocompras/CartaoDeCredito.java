cc9f64d356c8285135176d73dbd95f2d0b0752db
Matheus <mthsilva1998@gmail.com>
2024-08-28 01:08:02 -0300

    28/08/2024

--- src/aplicaocompras/CartaoDeCredito.java
+++ src/aplicaocompras/CartaoDeCredito.java
@@ -15,6 +15,16 @@ public class CartaoDeCredito {
         this.compras = new ArrayList<>();
     }
 
+    public boolean lancaCompra(Compra compra) {
+        if (this.saldo > compra.getValor()) {
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+
     public double getLimite() {
         return limite;
     }
