29c5a9744e0c09a4ded92565d78bbff6b12cfad4
Matheus <mthsilva1998@gmail.com>
2024-08-29 21:49:19 -0300

    29/08/2024

--- src/br/com/alura/screenmatch/app/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/app/PrincipalComBusca.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch.app;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,11 +24,16 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
 
 
+        String json = response.body();
+        System.out.println(json);
 
 
+        Gson gson = new Gson();
+
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
 
 
 
